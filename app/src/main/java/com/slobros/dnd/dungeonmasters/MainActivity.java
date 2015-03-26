ebcb3013a46133b2adee9727ce0907c57eaedea1
George Reyes <zypher452@gmail.com>
2015-03-25 22:43:40 -0700

    Added a refactored layout based on a tutorial I read. Still needs to be adapted to our purposes and a adapter that binds the views needs to be added. Step at a time...

--- app/src/main/java/com/slobros/dnd/dungeonmasters/MainActivity.java
+++ app/src/main/java/com/slobros/dnd/dungeonmasters/MainActivity.java
@@ -2,8 +2,11 @@ package com.slobros.dnd.dungeonmasters;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +15,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.cardList);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
     }
 
 
